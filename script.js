08eed130f61024bec61d502606ce9fbf6e85dcc7
humancoder1 <jhamanas4532@gmail.com>
2023-10-05 07:36:54 +0530

    added button and time elements to html and javascript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let start_btn =
+
+let hours = document.getElementById("hours-text");
+let minutes = document.getElementById("minutes-text");
+let seconds = document.getElementById("seconds-text");
+let milli_seconds = document.getElementById("milli_seconds-text");
+
+
+let start_button = document.getElementById("start-btn");
+let stop_button = document.getElementById("stop-btn");
+
+/*
+->start button ---> pause button ---> resume button
+->stop button ---> lap button ---> reset button
+-> hours element
+-> minute element
+-> seconds element
+-> miliseconds element
+*/
